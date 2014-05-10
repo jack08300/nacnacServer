21c3b0d46287b496d9bdbe5018bc7f91275f52d2
jay <jack08300@hotmail.com>
2014-05-10 17:04:51 -0400

    register and login

--- grails-app/controllers/nacnacserver/UserController.groovy
+++ grails-app/controllers/nacnacserver/UserController.groovy
@@ -1,5 +1,29 @@
 package nacnacserver
 
+import grails.converters.JSON
+
 class UserController {
     static scaffold = true
+
+    def loginOrRegister(String email, String password, boolean register){
+        def user = User.findByEmail(email)
+        Map result
+        if(user){
+            if(password == user.password){
+                result = [success: true, message: ""]
+            }else{
+                result = [success: false, message: "Wrong password"]
+            }
+        }else{
+            if(register){
+                new User(email: email, password: password).save(flush:true, failOnError: true)
+                result = [success: true, message: ""]
+            }else{
+                result = [success: false, register: true, message: "Please Register"]
+            }
+
+        }
+        println result
+        render result as JSON
+    }
 }
